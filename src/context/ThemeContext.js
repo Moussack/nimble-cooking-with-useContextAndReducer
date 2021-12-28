cf625c46aed4c100b9e6eef2c8700ce57ad849fa
Moussack <reywahyono@gmail.com>
2021-12-28 10:39:24 +0700

    lesson 97 done

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -5,3 +5,28 @@ export const ThemeContext = createContext();
 export function ThemeProvider({ children }) {
    return <ThemeContext.Provider value={{ color: 'teal' }}>{children}</ThemeContext.Provider>;
 }
+
+/* import { createContext, useReducer } from 'react';
+
+export const ThemeContext = createContext();
+
+const themeReducecr = (state, action) => {
+   switch (action.type) {
+      case 'CHANGE_COLOR':
+         return { ...state, color: action.payload };
+
+      default:
+         return state;
+   }
+};
+
+export function ThemeProvider({ children }) {
+   const [state, dispatch] = useReducer(themeReducer, { color: 'teal' });
+
+   const changeColor = (color) => {
+      dispatch({ type: 'CHANGE_COLOR', payload: color });
+   };
+
+   return <ThemeContext.Provider value={{ ...state, changeColor }}>{children}</ThemeContext.Provider>;
+}
+ */
