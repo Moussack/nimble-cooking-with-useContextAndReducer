ccb6d1719c34b37347f7dafebcfed0e9778e9cef
Moussack <reywahyono@gmail.com>
2022-01-17 13:57:51 +0700

    lesson 100 done

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -7,17 +7,24 @@ const themeReducer = (state, action) => {
       case 'CHANGE_COLOR':
          return { ...state, color: action.value };
 
+      case 'CHANGE_MODE':
+         return { ...state, mode: action.value };
+
       default:
          return state;
    }
 };
 
 export function ThemeProvider({ children }) {
-   const [state, dispatch] = useReducer(themeReducer, { color: '#58249c' });
+   const [state, dispatch] = useReducer(themeReducer, { color: '#58249c', mode: 'dark' });
 
    const changeColor = (color) => {
       dispatch({ type: 'CHANGE_COLOR', value: color });
    };
 
-   return <ThemeContext.Provider value={{ ...state, changeColor }}>{children}</ThemeContext.Provider>;
+   const changeMode = (mode) => {
+      dispatch({ type: 'CHANGE_MODE', value: mode });
+   };
+
+   return <ThemeContext.Provider value={{ ...state, changeColor, changeMode }}>{children}</ThemeContext.Provider>;
 }
